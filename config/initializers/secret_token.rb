c79612accd29acfdc32a3f877a4ccfbb273cb2cf
Duane R. Bailey <bailey.d.r@gmail.com>
2013-02-28 07:00:18 -0500

    Initial app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autoadvisor::Application.config.secret_token = '27ef7593eda4517bdab35a688ba1a3278f12864d6613ab907887f4049fd96c52f8eb81991fdff6129ca201f51b59c854cb6be570329562ec08a52cb1616e8860'
