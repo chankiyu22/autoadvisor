0fdd3484021031b7b338d61682aba1ab62165490
nconlon <nconlon@student.umass.edu>
2013-03-30 13:42:27 -0400

    better checks for transcripts & courses

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -10,33 +10,37 @@ class UploadsController < ApplicationController
     		directory = "public/uploadedTranscript"
     		path = File.join(directory, transcriptFile)
     		File.open(path, "wb") { |f| f.write(params[:file].read)}
-    		#callParser("public/uploadedTranscript/"+transcriptFile)
+    		#system(path/to/parser #{"public/uploadedTranscript/"+transcriptFile})
     		File.delete("public/uploadedTranscript/"+transcriptFile)
     		redirect_to :action => 'transcript', :controller => 'pages'
     	else
+    		#file field was blank
     		redirect_to :action => 'transcript', :controller => 'pages'
     	end
 	end
 
 	#Upload an individual course then redirect back to the transcript
-	#page. New course should be visible. does not check
+	#page. New course should be visible. Only uploads if all 4 fields 
+	#are filled in.
 	def upload_course
-		usernow = User.find(session[:'warden.user.user.key'][1][0])
-		year = params[:year]
-		courseName = params[:courseName]
-		courseCode = params[:courseCode]
-		grade = params[:grade]
-		usernow.past_courses << PastCourse.new(:year => year, :course_name => courseName, :course_code => courseCode, :grade => grade)
-		redirect_to :action =>'transcript', :controller => 'pages'
+		if (!params[:year].blank?)&&(!params[:courseName].blank?)&&(!params[:courseCode].blank?)&&(!params[:grade].blank?)
+			usernow = User.find(session[:'warden.user.user.key'][1][0])
+			usernow.past_courses << PastCourse.new(
+					:year => params[:year], 
+					:course_name => params[:courseName], 
+					:course_code => params[:courseCode], 
+					:grade => params[:grade])
+			redirect_to :action =>'transcript', :controller => 'pages'
+		else
+			#at least 1 field was left blank
+			redirect_to :action =>'transcript', :controller => 'pages'
+		end
 	end
 
 	#Deletes the selected course information from the users records.
 	def delete
 		usernow = User.find(session[:'warden.user.user.key'][1][0])
-
 		redirect_to :action=> 'transcript',:controller=>'pages'
-		
-
 	end
 
 end
