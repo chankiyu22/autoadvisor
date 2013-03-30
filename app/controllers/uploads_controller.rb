c6ccafbff3b1453633b66277c9a3c9916960c0f2
nconlon <nconlon@student.umass.edu>
2013-03-30 20:30:01 -0300

    Update uploads_controller.rb
    when a transcript is uploaded, it goes to public/uploadedTranscript .... you may need to create this file! it is uploaded, passed to parser, then deleted!

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,6 +3,9 @@ class UploadsController < ApplicationController
 	#Upload a transcirpt. if there is a file to upload temporaraly save it
 	#in "public/uploadedTranscript", then delete it after parsing. if no 
 	#file stay on same page.
+	
+	#!!!!!!!!YOU NEED TO CREATE public/uploadTranscript FOR THIS TO WORK!!!!!!!
+	
 	def upload_transcript
 		usernow = User.find(session[:'warden.user.user.key'][1][0])
 		if (!params[:file].blank?)
